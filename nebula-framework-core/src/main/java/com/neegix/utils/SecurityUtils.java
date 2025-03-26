ee6bd79560772e85297347aff6660f6930d283ce
kushu001 <yj_tao123@sina.com>
2025-03-26 16:11:35 +0800

    feat(SecurityUtils): 新建获取当前用户信息的工具，方便使用

---
+++ nebula-framework-core/src/main/java/com/neegix/utils/SecurityUtils.java
@@ -0,0 +1,29 @@
+package com.neegix.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * Created by IntelliJ IDEA (Community Edition)
+ * Thank you to JetBrains for their contributions to the vast number of developers
+ *
+ * @Author: kushu001
+ * @Date: 2025/03/26/13:49
+ * @Description:
+ */
+public class SecurityUtils {
+    private static Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public static <T> T getCurrentUser(Class<T> clazz) {
+        Authentication authentication = getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (clazz.isInstance(principal)) {
+                return clazz.cast(principal);
+            }
+        }
+        return null;
+    }
+}
